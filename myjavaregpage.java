9878a2723064ba0eb26b70b10a68bfe7ed8f480c
maha <maha@gmail.com>
2025-06-12 08:52:25 +0530

    my task comleted

--- myjavaregpage.java
+++ myjavaregpage.java
@@ -2,5 +2,10 @@
 2nd line of java code by Dev B
 3rd line of java code by Dev A
 4th line of java code by Dev B
-
 compelete manager work
+
+1st day of java code 
+2nd  day of java code
+3rd  day of java code
+4th  day of java code
+5th day of java code
